2cca5f9136c0699d4b8d036ef558c2e40af3d430
kaduprasad <kaduprasad99@gmail.com>
2023-03-25 01:56:05 +0530

    Updates

---
+++ precache-manifest.7e0e8e2eb089f977a39c536ef3037cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e77b865a7982c1b94f56",
+    "url": "/my-app/static/css/main.4a4ed845.chunk.css"
+  },
+  {
+    "revision": "e77b865a7982c1b94f56",
+    "url": "/my-app/static/js/main.e77b865a.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/my-app/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c14c305bc64c19d34b35b82582d3be0f",
+    "url": "/my-app/index.html"
+  }
+];
